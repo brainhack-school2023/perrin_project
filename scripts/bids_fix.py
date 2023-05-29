1bf7cc93d5f74d49e872f6cbc07d39286975368a
Isaac Perrin <isaac.perrin@imt-atlantique.net>
2023-05-29 13:05:48 -0400

    Update BIDS fix script and corresponding DataLad command

--- scripts/bids_fix.py
+++ scripts/bids_fix.py
@@ -1,14 +1,18 @@
+#!/usr/bin/env python3
+
 import os
 import glob
 import json
 import csv
 import pandas as pd
 
+from pathlib import Path
+
 # It is recommended to craft the command such that it can run in the root directory of the dataset that the command will be recorded in. 
 # However, as long as the command is executed somewhere underneath the dataset root, 
 # the exact location will be recorded relative to the dataset root.
 
-# dataset_dir = 'data/ds000201/'
+# dataset_dir = "data/ds000201/"
 
 ## Error 1: [Code 55] JSON_SCHEMA_VALIDATION_ERROR
 # Invalid JSON file. The file is not formatted according the schema.
@@ -18,15 +22,16 @@ import pandas as pd
 #   Evidence: .CogAtlasID should match format "uri"
 
 # Fix modify as url (better than just delete "CogAtlasID") (in root files and not leaves):
-def fix_json_schema():
-    task_json_files = sorted(glob.glob('task-*_bold.json'))#os.path.join(dataset_dir, 'task-*_bold.json')))
+def fix_json_schema(input_path):
+    relative_path = os.path.join(input_path, "task-*_bold.json")
+    task_json_files = sorted(glob.glob(relative_path))
     for filename in task_json_files:
         # Read .json file
         with open(filename, 'r') as ff:
             data = json.load(ff)
-            if 'CogAtlasID' in data.keys():
-                new_format = 'https://www.cognitiveatlas.org/concept/id/' + data['CogAtlasID'] + '/'
-                data['CogAtlasID'] = new_format
+            if "CogAtlasID" in data.keys():
+                new_format = "https://www.cognitiveatlas.org/concept/id/" + data["CogAtlasID"] + "/"
+                data["CogAtlasID"] = new_format
         with open(filename, 'w') as ff:
             json_object = json.dumps(data, indent=3)
             ff.write(json_object)
@@ -37,7 +42,7 @@ def fix_json_schema():
 # Other sources:
 #   BIDS Validator error - [Code 55] JSON_SCHEMA_VALIDATION_ERROR 
 #   https://neurostars.org/t/bids-validator-error-code-55-json-schema-validation-error-effectiveechospacing/3704
-#   CogAtlasID error for json files that don't contain a CogAtlasID field (https://github.com/bids-standard/bids-validator/issues/1139)
+#   CogAtlasID error for json files that don"t contain a CogAtlasID field (https://github.com/bids-standard/bids-validator/issues/1139)
 #   (code: 55 - JSON_SCHEMA_VALIDATION_ERROR) #1104 (https://github.com/bids-standard/bids-validator/issues/1104)
 
 
@@ -56,12 +61,12 @@ def fix_json_schema():
 #   Line: 3 row 2: n/a 0.395
 
 # Fix for errors 2 and 3: merge even and odd rows which were separated before
-def fix_beh_tsv():
-    relative_path = 'sub-*/ses-*/beh/sub-*_task-PVT_events.tsv'
+def fix_beh_tsv(input_path):
+    relative_path = os.path.join(input_path, "sub-*/ses-*/beh/sub-*_task-PVT_events.tsv")
     beh_tsv_files = sorted(glob.glob(relative_path)) # os.path.join(dataset_dir, relative_path)
     for filename in beh_tsv_files[0:1]:
         with open(filename, 'r') as file:
-            reader = csv.reader(file, delimiter='\t')
+            reader = csv.reader(file, delimiter="\t")
             headers = next(reader)
             reader_df = pd.DataFrame(reader)
             even = reader_df[reader_df.index % 2 == 0].reset_index(drop=True)
@@ -69,7 +74,7 @@ def fix_beh_tsv():
             new_df = pd.concat([even.iloc[:, :1], odd.iloc[:, 1:]], axis=1)
             new_df.columns = headers
             # Save the modified .tsv file
-            new_df.to_csv(filename, sep='\t')
+            new_df.to_csv(filename, sep="\t")
 
 ## Error 4: [Code 90] SIDECAR_WITHOUT_DATAFILE
 # A json sidecar file was found without a corresponding data file
@@ -80,24 +85,31 @@ def fix_beh_tsv():
 ## Error 5: [Code 129] SCANS_FILENAME_NOT_MATCH_DATASET
 # The filename in scans.tsv file does not match what is present in the BIDS dataset.
 # Fix: remove lines fieldmap, imaginary, real
-def fix_scans_filename():
-    relative_path = 'sub-*/ses-*/sub-*_ses-*_scans.tsv'
+def fix_scans_filename(input_path):
+    relative_path = os.path.join(input_path, "sub-*/ses-*/sub-*_ses-*_scans.tsv")
     beh_tsv_files = sorted(glob.glob(relative_path)) # os.path.join(dataset_dir, relative_path)
     for filename in beh_tsv_files[0:1]:
-        with open(filename, 'r') as file:
-            df = pd.read_csv(filename, delimiter='\t')
-            values_to_remove = ['fieldmap', 'imaginary', 'real']
+        with open(filename, "r") as file:
+            df = pd.read_csv(filename, delimiter="\t")
+            values_to_remove = ["fieldmap", "imaginary", "real"]
             pattern = '|'.join(values_to_remove)
-            new_df = df.loc[~df['filename'].str.contains(pattern, case=False)].reset_index(drop=True)
+            new_df = df.loc[~df["filename"].str.contains(pattern, case=False)].reset_index(drop=True)
             # Save the modified .tsv file
-            new_df.to_csv(filename, sep='\t')
-            # new_df.to_csv(filename[:-4] + '_bis.tsv', sep='\t')
+            new_df.to_csv(filename, sep="\t")
+            # new_df.to_csv(filename[:-4] + "_bis.tsv", sep="\t")
 
-# Fix error 1
-fix_json_schema()
+def main(repo_path):
+    input_path = repo_path / "input"
+    # Fix error 1
+    fix_json_schema(input_path)
+    # Fix errors 2 and 3
+    fix_beh_tsv(input_path)
+    # Fix error 5
+    fix_scans_filename(input_path)
 
-# Fix errors 2 and 3
-fix_beh_tsv()
 
-# Fix error 5
-fix_scans_filename()
+if __name__ == "__main__":
+    repo_path = Path(__file__).parent.parent
+    main(repo_path)
+
+
